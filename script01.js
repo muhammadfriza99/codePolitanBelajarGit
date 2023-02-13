2b3a8996181c62cc7d9e5f83b19fadff56b677bc
frizamuhammad99 <frizamuhammad99@gmail.com>
2023-02-13 19:32:44 +0700

    mengubah tampilan string

--- script01.js
+++ script01.js
@@ -1,10 +1,9 @@
-console.log('Friza ganteng sekali');
-console.log('Friza ganteng sekali');
+console.log('Vegy Destianty');
+console.log('Vegy Destianty');
 console.log('Friza ganteng sekali');
 
 console.log('Vegy Destianty');
-console.log('Vegy Destianty');
-console.log('Vegy Destianty');
+
 
 let add = (a,b) => a+b;
 console.log(add(9,0));
