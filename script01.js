35d27281871fb138608770547d75c6a9e6c5ea4e
frizamuhammad99 <frizamuhammad99@gmail.com>
2023-02-13 19:11:30 +0700

    menambah fitur 1

--- script01.js
+++ script01.js
@@ -5,3 +5,6 @@ console.log('Friza ganteng sekali');
 console.log('Vegy Destianty');
 console.log('Vegy Destianty');
 console.log('Vegy Destianty');
+
+let add = (a,b) => a+b;
+console.log(add(9,0));
