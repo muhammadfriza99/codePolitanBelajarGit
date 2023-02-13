49e78c3d4f93a7e912730540f629c60de9119aa2
frizamuhammad99 <frizamuhammad99@gmail.com>
2023-02-13 19:47:41 +0700

    menyelesaikan konflik

--- script01.js
+++ script01.js
@@ -3,9 +3,5 @@ console.log('Vegy Destianty');
 console.log('Friza ganteng sekali');
 
 console.log('Vegy Destianty');
-
-
-let add = (a,b) => a+b;
-console.log(add(9,0));
-let minus = (a,b) => a-b;
-console.log(add(9,9));
+console.log('Vegy Destianty');
+console.log('Vegy Destianty');
